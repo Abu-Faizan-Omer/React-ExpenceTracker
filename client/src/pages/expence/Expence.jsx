fca1b288226abb0694507a8e9d16ba144bd15844
Abu-Faizan-Omer <abufaizan1997@gmail.com>
2025-12-08 19:55:09 +0530

    logout

--- client/src/pages/expence/Expence.jsx
+++ client/src/pages/expence/Expence.jsx
@@ -1,12 +1,23 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link,useNavigate } from 'react-router-dom'
 
 export const Expence = () => {
+  const navigate = useNavigate();
+
+
+  const handleLogout=()=>{
+    localStorage.clear();
+    navigate('/login');
+  }
   return (
     <>
     <div>Welcome to Expence page</div>
     <h4>Your profile is Incomplete Click below!!!</h4>
     <Link to="/details">Complete Details</Link> 
+
+    <div>
+      <button onClick={handleLogout} type='button'>Logout</button>
+    </div>
     </>
     
   )
